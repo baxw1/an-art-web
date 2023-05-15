a721ea820fc7cf655f852db7ceadf64ba5b6d774
不爱洗碗 <870116482@qq.com>
2023-05-15 23:33:08 +0800

    文字

--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -5,10 +5,20 @@
       <div class="animate__animated textframe"></div>
       <div class="text-container" @click="pagetoMenu">
         <div class="typed-out">
-          <div>hahahhahhhhhhhhhhhhhhrrrrrrrrrr</div>
-          <div>hahahhahhhhhhhhhhhhhhwwwwwwhhhh</div>
-          <div>hahahhahhhhhhhhhhhh1111111111hh</div>
-          <div>yyyyyyyy</div>
+          <div>欢迎来到互联网语言考古计划2050!</div>
+          <div>二十年前的互联网语言是什么样的？生活中受到挫败的底层人群在特殊的话语世界（特别是网络空间）里却</div>
+          <div>现得异常活跃，其现实生存空间受到挤压转而以流行语言为宣泄出口,借此表达维权意图和对于优势阶层的愤懑不满,</div>
+          <div>虽偏于情绪化却折射出底层参与范式的新取向,即底层话语权受到了一定的限制,为了突破这个壁垒，底层人群通过一</div>
+          <div>套“在野的话语体系”来宣示自己的诉求。</div>
+          <div> </div>
+          <div>互联网语言不同于严谨宏大的官方语篇,不具备开端、高潮、结局的完整情节,常常以一句话的简单描述,将“人</div>
+          <div>物、场景、行为”等要素融入日常生活的微型叙事中。 微型叙事使流行体话语的意义指向实用而直观,“反强权、讽社</div>
+          <div>会、哀民生” 等价值取向十分清晰。</div>
+          <div> </div>
+          <div>一方面是对主流意识形态宏大叙事的消解,另一方面,又彰显了民间意识形态在语言和记忆层面上的推进,正如利奥</div>
+          <div>塔所言,小叙事将可能被遮蔽的社会事实点亮,从而引入社会大众的历史叙事,并获得被讨论的资格,它的流行与传播,最</div>
+          <div>大化了群体认知,从而锚定了社会记忆。</div>
+
         </div>
       </div>
       <div class="topic2" v-if="showTopic"></div>
@@ -87,41 +97,93 @@ onMounted(()=>{
   display: inline-block;
 }
 .typed-out {
-  --animation-duration:5000ms;
+  --animation-duration:2000ms;
   --animation-iteration:1;
+  --start: 282px;
+  --line-space:30px
 }
 .typed-out div{
   position: absolute;
   z-index: 17;
   overflow: hidden;
   /* border-right: .15em solid gray; */
-  font-size: 3rem;
+  font-size: 1rem;
   color: aliceblue;
   width: 0px;
+  height: 20px;
   animation: typing var(--animation-duration) linear var(--animation-iteration);
   animation-fill-mode: forwards;
+  text-align: start;
 }
 .typed-out div:nth-child(1){
-  left: 377px;
-  top: 282px;
-  height: 46px;
+  left: 417px;
+  top: var(--start);
+  height: 20px;
   animation-delay: 1500ms;
 }
 .typed-out div:nth-child(2){
-  left: 377px;
-  top: 382px;
+  left: 417px;
+  top: calc(1*var(--line-space) + var(--start));
   animation-delay: calc(var(--animation-duration) + 1500ms);
 }
 .typed-out div:nth-child(3){
   left: 377px;
-  top: 482px;
+  top: calc(2*var(--line-space) + var(--start));
   animation-delay: calc(2*var(--animation-duration) + 1500ms);
 }
 .typed-out div:nth-child(4){
   left: 377px;
-  top: 582px;
+  top: calc(3*var(--line-space) + var(--start));
   animation-delay: calc(3*var(--animation-duration) + 1500ms);
 }
+.typed-out div:nth-child(5){
+  left: 377px;
+  top: calc(4*var(--line-space) + var(--start));
+  animation-delay: calc(4*var(--animation-duration) + 1500ms);
+}
+.typed-out div:nth-child(6){
+  left: 417px;
+  top: calc(4*var(--line-space) + var(--start));
+  animation-duration: 0.5s;
+  animation-delay: calc(5*var(--animation-duration) + 1500ms);
+}
+.typed-out div:nth-child(7){
+  left: 417px;
+  top: calc(5*var(--line-space) + var(--start));
+  animation-delay: calc(5*var(--animation-duration) + 2000ms);
+}
+.typed-out div:nth-child(8){
+  left: 377px;
+  top: calc(6*var(--line-space) + var(--start));
+  animation-delay: calc(6*var(--animation-duration) + 0.5s + 1500ms);
+}
+.typed-out div:nth-child(9){
+  left: 377px;
+  top: calc(7*var(--line-space) + var(--start));
+  animation-delay: calc(7*var(--animation-duration) + 0.5s + 1500ms);
+}
+.typed-out div:nth-child(10){
+  left: 417px;
+  top: calc(8*var(--line-space) + var(--start));
+  animation-duration: 0.5s;
+  animation-delay: calc(8*var(--animation-duration) + 2000ms);
+}
+.typed-out div:nth-child(11){
+  left: 417px;
+  top: calc(8*var(--line-space) + var(--start));
+  animation-delay: calc(8*var(--animation-duration) + 2500ms);
+}
+.typed-out div:nth-child(12){
+  left: 377px;
+  top: calc(9*var(--line-space) + var(--start));
+  animation-delay: calc(9*var(--animation-duration) + 2500ms);
+}
+.typed-out div:nth-child(13){
+  left: 377px;
+  top: calc(10*var(--line-space) + var(--start));
+  animation-delay: calc(10*var(--animation-duration) + 2500ms);
+}
+
 @keyframes typing {
   from { width: 0px }
   to { width: 794px; }
